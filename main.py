f2c3b399c76689fc93df4df46e20fe079ad81b52
Joseph <joseph.el-hachem@student.ecp.fr>
2020-06-04 18:43:08 +0300

    commiting main

--- main.py
+++ main.py
@@ -18,27 +18,26 @@ parser.add_argument('--save_dir', type=str, required=False, default=None,
                     help='directory to save images on which gradcam was applied')
 parser.add_argument('--n_images', type=int, default=1,
                     help='number of images used for inference')
-
 parser.add_argument('--show', default=False, action='store_true',
                     help='set True to show plots. Default is True')
 
 args = parser.parse_args()
 print(args)
-# if args.imageNet_labels:
-#     http = urllib3.PoolManager()
-#     url ="https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
-#     idx2label_path = "imagenet1000_labels.txt"
-#     with http.request('GET', url, preload_content=False) as r, open(idx2label_path, 'wb') as out_file:
-#         shutil.copyfileobj(r, out_file)
-# else:
-#     idx2label_path = args.labels_path
-#
-#
-# model = torch.load(args.model_path)
-#
-# layer = model.features[28]
-# grad_cam = GradCam(model, layer, args.images_path, idx2label_path, save_dir=args.save_dir, show=args.show)
-# grad_cam.lunch_grad_cam(n_images=args.n_images)
-# grad_cam.plot_grad_cam()
+if args.imageNet_labels:
+    http = urllib3.PoolManager()
+    url ="https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt"
+    idx2label_path = "imagenet1000_labels.txt"
+    with http.request('GET', url, preload_content=False) as r, open(idx2label_path, 'wb') as out_file:
+        shutil.copyfileobj(r, out_file)
+else:
+    idx2label_path = args.labels_path
+
+
+model = torch.load(args.model_path)
+
+layer = model.features[28]
+grad_cam = GradCam(model, layer, args.images_path, idx2label_path, save_dir=args.save_dir, show=args.show)
+grad_cam.lunch_grad_cam(n_images=args.n_images)
+grad_cam.plot_grad_cam()
 
 print('Done, Eex')
